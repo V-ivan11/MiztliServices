d5e210657b639d7377e663d2e9144d9a7c1a14be
Ivan V <ivanverduzco11@gmail.com>
2022-04-04 20:46:01 -0500

    Fuentes y paleta de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'lexend': ['Lexend', 'sans-serif'],
+        'ballad': ['BALLAD', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
