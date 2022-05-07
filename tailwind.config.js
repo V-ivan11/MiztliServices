5d6f9ee196198d371c9871edac7adde6bc847b43
Ivan Verduzco <ivanverduzco11@gmail.com>
2022-05-07 15:16:06 -0500

    countinuacion del diseño

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
       fontFamily:{
         'lexend': ['Lexend', 'sans-serif'],
         'ballad': ['BALLAD', 'sans-serif']
+      },
+      backgroundImage:{
+        'img1': "url('img/palmira.jpg')",
+        'img2': "url('img/ahr.jpg')",
+        'img3': "url('img/stfe.jpg')"
       }
     },
   },
