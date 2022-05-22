9bd96b8976ef451e24273b49e4a55114a60619b5
Ivan Verduzco <ivanverduzco11@gmail.com>
2022-05-21 21:50:26 -0500

    SEO y planes de redes sociales.

--- src/js/functions.js
+++ src/js/functions.js
@@ -16,35 +16,3 @@ form.addEventListener('submit', (e) => {
     form.reset()
     
 });
-
-
-
-function cambiarCategoria(option) {
-    var btn_sites = document.getElementById("btn_sites");
-    var btn_media = document.getElementById("btn_media");
-
-    var cardLeft = document.getElementById("cardLeft");
-    var cardMain = document.getElementById("cardMain");
-    var cardRight = document.getElementById("cardRight");
-
-    var on = "inline-block p-4 text-myBlue rounded-t-lg border-b-2 border-myBlue active";
-    var off = "inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:border-gray-300 hover:text-gray-300"
-
-    var planWebBasic = ""
-
-    if (option == 1) {
-        btn_sites.className = on;
-        btn_media.className = off;
-
-        cardLeft.innerHTML = "web";
-        cardMain.innerHTML = "web";
-        cardRight.innerHTML = "web";
-    } else {
-        btn_sites.className = off;
-        btn_media.className = on;
-
-        cardLeft.innerHTML = "media";
-        cardMain.innerHTML = "media";
-        cardRight.innerHTML = "media";
-    }
-}
