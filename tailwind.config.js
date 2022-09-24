a618e57646f57d0c52fb53427b31bc86af144579
Ivan Verduzco <ivanverduzco11@gmail.com>
2022-09-23 20:51:31 -0500

    base página sobre la empresa

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,6 @@ module.exports = {
         'lexend': ['Lexend', 'sans-serif'],
         'ballad': ['BALLAD', 'sans-serif']
       },
-      backgroundImage:{
-        'img1': "url('img/palmira.jpg')",
-        'img2': "url('img/ahr.jpg')",
-        'img3': "url('img/stfe.jpg')"
-      },
       colors:{
         'primary': '#54428E',
         'secondary': '#FFF',
